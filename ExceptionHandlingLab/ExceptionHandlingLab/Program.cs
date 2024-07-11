62f9a3735d18918b7bbde430a548953ff60f01be
Travis-DV <tfindley2@my.stlcc.edu>
2024-07-11 00:09:40 -0500

    Added Code

---
+++ ExceptionHandlingLab/ExceptionHandlingLab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Validator;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] numbersToAdv = Validator.Validator.GetIntArray("Enter numbers separated by spaces to calculate the average:");
+        Console.WriteLine("Calculating the average... ");
+        Console.WriteLine(CalculateAverage(numbersToAdv).ToString());
+    }
+
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            Console.WriteLine("Numbers equals null or length is 0");
+            return 0.0;
+        }
+
+        double sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        return sum / numbers.Length;
+    }
+}
